23ff3c56d3e6b9d8d99764e976b4a97bb3eb4a64
Lucas Lima <id.lucas@outlook.com.br>
2023-05-02 23:05:36 -0300

    refactoring

--- src/pages/escala-completa.tsx
+++ src/pages/escala-completa.tsx
@@ -1,159 +1,158 @@
+import { getTitlePositionScale } from "@/utils/get-title-position-scale";
 import { getDayWeek } from "@/utils/get-week-day";
 import Link from "next/link";
 
-export default function FulllScale() {
+export default function FullScale() {
   const data = [
     {
       id: "1",
       date: "04/05/2023",
       scale: {
-        "🎙️ MINISTRANTE": "Leticia",
-        "🎙️ APOIO": "Augusto",
-        "🎸 VIOLÃO": "Lucas",
-        "🎸 GUITARRA": "Jhon",
-        "🎸 BAIXO": "Isaque",
-        "🥁 BATERIA": "Caique",
-        "🎛️ SONOPLASTA": "Carlos",
+        minister: "Leticia",
+        support: "Augusto",
+        guitar: "Lucas",
+        guitarEletric: "Jhon",
+        down: "Isaque",
+        drum: "Caique",
+        soundEngineer: "Carlos",
       },
     },
     {
       id: "2",
       date: "06/05/2023",
       scale: {
-        "🎙️ MINISTRANTE": "Augusto",
-        "🎙️ APOIO": "Rebeca",
-        "🎸 VIOLÃO": "Elizandra",
-        "🎸 BAIXO": "Isaque",
-        "🥁 BATERIA": "Caique",
+        minister: "Augusto",
+        support: "Rebeca",
+        guitar: "Elizandra",
+        down: "Isaque",
+        drum: "Caique",
       },
     },
     {
       id: "3",
       date: "07/05/2023",
       scale: {
-        "🎙️ MINISTRANTE": "Menesteu",
-        "🎙️ APOIO": "Rayane",
-        "🎸 VIOLÃO": "Elizandra",
-        "🎸 GUITARRA": "Jhon",
-        "🎸 BAIXO": "Isaque",
-        "🥁 BATERIA": "Caique",
-        "🎛️ SONOPLASTA": "Jhonne",
+        minister: "Menesteu",
+        support: "Rayane",
+        guitar: "Elizandra",
+        guitarEletric: "Jhon",
+        down: "Isaque",
+        drum: "Caique",
+        soundEngineer: "Jhonne",
       },
     },
     {
       id: "4",
       date: "11/05/2023",
       scale: {
-        "🎙️ MINISTRANTE": "Pra. Mayne",
-        "🎙️ APOIO": "Lucas",
-        "🎸 VIOLÃO": "Lucas",
-        "🎸 GUITARRA": "Jhon",
-        "🎸 BAIXO": "Isaque",
-        "🥁 BATERIA": "Caique",
-        "🎛️ SONOPLASTA": "Carlos",
+        minister: "Pra. Mayne",
+        support: "Lucas",
+        guitar: "Lucas",
+        guitarEletric: "Jhon",
+        down: "Isaque",
+        drum: "Caique",
+        soundEngineer: "Carlos",
       },
     },
     {
       id: "5",
       date: "13/05/2023",
       scale: {
-        "🎙️ MINISTRANTE": "Rayane",
-        "🎙️ APOIO": "Augusto",
-        "🎸 VIOLÃO": "Elizandra",
-        "🎸 BAIXO": "Isaque",
-        "🥁 BATERIA": "Caique",
+        minister: "Rayane",
+        support: "Augusto",
+        guitar: "Elizandra",
+        down: "Isaque",
+        drum: "Caique",
       },
     },
     {
       id: "6",
       date: "14/05/2023",
       scale: {
-        "🎙️ MINISTRANTE": "Menesteu",
-        "🎙️ APOIO": "Rebeca",
-        "🎸 VIOLÃO": "Lucas",
-        "🎸 GUITARRA": "Jhon",
-        "🎸 BAIXO": "Isaque",
-        "🥁 BATERIA": "Caique",
-        "🎛️ SONOPLASTA": "Jhonne",
+        minister: "Menesteu",
+        support: "Rebeca",
+        guitar: "Lucas",
+        guitarEletric: "Jhon",
+        down: "Isaque",
+        drum: "Caique",
+        soundEngineer: "Jhonne",
       },
     },
     {
       id: "7",
       date: "18/05/2023",
       scale: {
-        "🎙️ MINISTRANTE": "Leticia",
-        "🎙️ APOIO": "Augusto",
-        "🎸 VIOLÃO": "Lucas",
-        "🎸 GUITARRA": "Jhon",
-        "🎸 BAIXO": "Isaque",
-        "🥁 BATERIA": "Caique",
-        "🎛️ SONOPLASTA": "Carlos",
+        minister: "Leticia",
+        support: "Augusto",
+        guitar: "Lucas",
+        guitarEletric: "Jhon",
+        down: "Isaque",
+        drum: "Caique",
+        soundEngineer: "Carlos",
       },
     },
     {
       id: "8",
       date: "20/05/2023",
       scale: {
-        "🎙️ MINISTRANTE": "Rebeca",
-        "🎙️ APOIO": "Augusto",
-        "🎸 VIOLÃO": "Elizandra",
-        "🎸 BAIXO": "Isaque",
-        "🥁 BATERIA": "Caique",
+        minister: "Rebeca",
+        support: "Augusto",
+        guitar: "Elizandra",
+        down: "Isaque",
+        drum: "Caique",
       },
     },
     {
       id: "9",
       date: "21/05/2023",
       scale: {
-        "🎙️ MINISTRANTE": "Pra. Mayne",
-        "🎙️ APOIO": "Menesteu",
-        "🎸 VIOLÃO": "Lucas",
-        "🎸 GUITARRA": "Jhon",
-        "🎸 BAIXO": "Isaque",
-        "🥁 BATERIA": "Caique",
-        "🎛️ SONOPLASTA": "Jhonne",
+        minister: "Pra. Mayne",
+        support: "Menesteu",
+        guitar: "Lucas",
+        guitarEletric: "Jhon",
+        down: "Isaque",
+        drum: "Caique",
+        soundEngineer: "Jhonne",
       },
     },
     {
       id: "10",
       date: "25/05/2023",
       scale: {
-        "🎙️ MINISTRANTE": "Lucas",
-        "🎙️ APOIO": "Rebeca",
-        "🎸 VIOLÃO": "Lucas",
-        "🎸 GUITARRA": "Jhon",
-        "🎸 BAIXO": "Isaque",
-        "🥁 BATERIA": "Caique",
-        "🎛️ SONOPLASTA": "Jhonne",
+        minister: "Lucas",
+        support: "Rebeca",
+        guitar: "Lucas",
+        guitarEletric: "Jhon",
+        down: "Isaque",
+        drum: "Caique",
+        soundEngineer: "Jhonne",
       },
     },
     {
       id: "11",
       date: "27/05/2023",
       scale: {
-        "🎙️ MINISTRANTE": "Rebeca",
-        "🎙️ APOIO": "Augusto",
-        "🎸 VIOLÃO": "Elizandra",
-        "🎸 BAIXO": "Isaque",
-        "🥁 BATERIA": "Caique",
+        minister: "Rebeca",
+        support: "Augusto",
+        guitar: "Elizandra",
+        down: "Isaque",
+        drum: "Caique",
       },
     },
     {
       id: "11",
       date: "28/05/2023",
       scale: {
-        "🎙️ MINISTRANTE": "Menesteu",
-        "🎙️ APOIO": "Rayane",
-        "🎸 VIOLÃO": "Elizandra",
-        "🎸 GUITARRA": "Jhon",
-        "🎸 BAIXO": "Isaque",
-        "🥁 BATERIA": "Caique",
-        "🎛️ SONOPLASTA": "Jhonne",
+        minister: "Menesteu",
+        support: "Rayane",
+        guitar: "Elizandra",
+        guitarEletric: "Jhon",
+        down: "Isaque",
+        drum: "Caique",
+        soundEngineer: "Jhonne",
       },
     },
   ];
-
-
 
   return (
     <main className="min-h-screen max-w-5xl pt-16 pb-28 px-4">
@@ -170,12 +169,12 @@ export default function FulllScale() {
               <b className="text-xl">{getDayWeek(item.date)}</b>
               <p>{item.date}</p>
             </h1>
-            {Object.entries(item.scale).map(([key, obj]) => (
+            {Object.entries(item.scale).map(([key, obj]: any) => (
               <p
                 key={key}
                 className="bg-violet-700 border-b-2 py-2 px-4 text-lg"
               >
-                {key} | <b>{obj}</b>
+                {getTitlePositionScale(key)} | <b>{obj}</b>
               </p>
             ))}
           </div>
